f712e21ec3c12d97da3252546949fdf0a36a424b
Richard Wills <richardwills90@gmail.com>
2014-05-30 01:26:52 +0100

    Can now exclude by resource or template.

--- core/components/hanzel/elements/snippets/snippet.hanzel.php
+++ core/components/hanzel/elements/snippets/snippet.hanzel.php
@@ -7,17 +7,38 @@ $mainCrumbTpl =$modx->getOption('mainCrumbTpl', $scriptProperties, null);
 $firstCrumbTpl =$modx->getOption('firstCrumbTpl', $scriptProperties, null);
 $currentCrumbTpl =$modx->getOption('currentCrumbTpl', $scriptProperties, null);
 
-$resource = $modx->getOption('resource', $scriptProperties, $modx->resource->get('id'));
-$includeHome = $modx->getOption('includeHome', $scriptProperties, true);
+$currentResource = $modx->getOption('currentResource', $scriptProperties, $modx->resource->get('id'));
 $siteStart = $modx->getOption('siteStart', $scriptProperties, $modx->getOption('site_start', null, 'default'));
 
-$parentList = $modx->getParentIds($resource);
-$parentList = array_reverse($parentList);
-array_push($parentList, $resource);
+$includeHome = $modx->getOption('includeHome', $scriptProperties, 'true');
+$excludeResources = $modx->getOption('excludeResources', $scriptProperties);
 
-if ($includeHome && !in_array($siteStart, $parentList)){
+if (isset($excludeResources)){$excludeResources = explode(',', $excludeResources);} 
+	else {$excludeResources = Array();}
+if (isset($excludeTemplates)){$excludeTemplates = explode(',', $excludeTemplates);} 
+	else {$excludeTemplates = Array();}
+
+array_push($excludeResources, '0');
+if ($includeHome == 'false'){
+	array_push($excludeResources, $siteStart);
+}
+
+
+//$excludeTemplates = $modx->getOption('excludeTemplates', $scriptProperties);
+
+
+
+$parentList = $modx->getParentIds($currentResource);
+$parentList = array_reverse($parentList);
+array_push($parentList, $currentResource);
+$parentList = array_diff($parentList, $excludeResources);
+
+if ($includeHome == 'true' && in_array($siteStart, $parentList) == false){
 	array_unshift($parentList, $siteStart);
 }
+
+
+
 
 $output = "<ul class=\"breadcrumb\">";
 
@@ -27,20 +48,21 @@ $vars[$tvPrefix."firstCrumbClass"] = $firstCrumbClass;
 $vars[$tvPrefix."currentCrumbClass"] = $currentCrumbClass;
 
 foreach ($parentList as $parent){
-	if ($parent != 0){
-		$object = $modx->getObject('modResource', $parent);
+
+	$object = $modx->getObject('modResource', $parent);
+	if (in_array($object->get('template'), $excludeTemplates) == true){
 		$vars[$tvPrefix."pagetitle"] = $object->get('pagetitle');
 		$vars[$tvPrefix."url"] = $modx->makeUrl($parent, "", "", "full");
 		
 		if ($parent == reset($parentList)){ //First
 			$output .= "<li class=\"".$firstCrumbClass."\"><a href=\"".$vars[$tvPrefix."url"]."\">".$vars[$tvPrefix."pagetitle"]."</a></li>";
-		} else if ($parent == $resource) { //Current
+		} else if ($parent == $currentResource) { //Current
 			$output .= "<li class=\"".$currentCrumbClass."\"><a href=\"".$vars[$tvPrefix."url"]."\">".$vars[$tvPrefix."pagetitle"]."</a></li>";	
 		} else { //Default
 			$output .= "<li class=\"".$mainCrumbClass."\"><a href=\"".$vars[$tvPrefix."url"]."\">".$vars[$tvPrefix."pagetitle"]."</a></li>";
-		}
-		
-	}
+		}	
+	}	
+	
 }
 
 	//$output .= "<li class=\"currentPage\"><a href=\"".$modx->makeUrl($modx->resource->get('id'), "", "", "full")."\">".$modx->resource->get('pagetitle')."</a></li>";
